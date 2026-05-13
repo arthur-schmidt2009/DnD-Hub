3b06d42efb6bc3fa61d233bd4226fb26e5e56316
arthur-schmidt2009 <arthur.valentin09@gmail.com>
2026-05-13 16:21:37 +0200

    Update server log message with URL

--- server.js
+++ server.js
@@ -3,25 +3,19 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
-
 const io = new Server(server);
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-
     console.log("Spieler verbunden");
 
     socket.on("rollDice", (data) => {
-
         io.emit("diceResult", data);
-
     });
-
 });
 
 server.listen(3000, () => {
-    console.log("Server l‰uft auf Port 3000");
+    console.log("Server l√§uft auf http://localhost:3000");
 });
