5c07f0ecbaed3175e2ec014add2bcfc07540d0c0
arthur-schmidt2009 <arthur.valentin09@gmail.com>
2026-05-13 16:01:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+﻿function rollDice(player, sides) {
+
+    const result = Math.floor(Math.random() * sides) + 1;
+
+    const feed = document.getElementById("feed");
+
+    const entry = document.createElement("div");
+
+    entry.classList.add("feed-entry");
+
+    if (sides === 20 && result === 20) {
+        entry.classList.add("crit");
+    }
+
+    if (sides === 20 && result === 1) {
+        entry.classList.add("fail");
+    }
+
+    entry.innerHTML = `
+        <strong>${player}</strong> würfelt D${sides}<br>
+        Ergebnis: <strong>${result}</strong>
+    `;
+
+    feed.prepend(entry);
+}
