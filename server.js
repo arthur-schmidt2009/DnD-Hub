f9bceba093e73d635cd4c1661b212a44dab2ebbe
arthur-schmidt2009 <arthur.valentin09@gmail.com>
2026-05-13 16:14:04 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+
+    console.log("Spieler verbunden");
+
+    socket.on("rollDice", (data) => {
+
+        io.emit("diceResult", data);
+
+    });
+
+});
+
+server.listen(3000, () => {
+    console.log("Server läuft auf Port 3000");
+});
